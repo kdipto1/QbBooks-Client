15251e3b3deec7bd006706bb3f0f3a21edb64502
kdipto1 <kdipto@outlook.com>
2022-09-10 14:38:59 +0600

    firebase connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './Pages/Home/Home';
+import SocialLogin from './Pages/Login/SocialLogin';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <SocialLogin/>
       hello world
     </div>
   );
---
+++ src/Pages/Login/SocialLogin.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import auth from '../../firebase.init';
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+const SocialLogin = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  // let froms = location.state?.from?.pathname || "/";
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  // useEffect(() => {
+  //   if (user) {
+  //     toast.success("Login Successful");
+  //     // console.log(user1);
+  //     const url = "https://stock-world-server.herokuapp.com/login";
+  //     axios
+  //       .post(url, { email: user?.email })
+  //       .then((response) => {
+  //         const { data } = response;
+  //         localStorage.setItem("accessToken", data.token);
+  //         localStorage.setItem("email", user?.email);
+  //         // console.log(data);
+  //         navigate(from, { replace: true });
+  //       })
+  //       .catch(function (error) {
+  //         toast.error(error.message);
+  //         console.log(error);
+  //       });
+  //   }
+  //   if (loading) {
+  //     return;
+  //   }
+  //   if (error) {
+  //     toast.error(error?.message);
+  //   }
+  // }, [from, user, navigate, error, loading]);
+  return (
+    <section>
+      <button
+        onClick={() => signInWithGoogle()}
+        className="btn btn-wide hover:btn-primary"
+      >
+        {" "}
+        Continue With google
+      </button>
+    </section>
+  );
+};
+
+export default SocialLogin;
